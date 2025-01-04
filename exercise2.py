f060ebfd5fdd58a392e677f4a891426d27f50c6e
Helio Edgar <hecoelho99@gmail.com>
2025-01-04 18:13:49 +0100

    Version 3

---
+++ exercise2.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um numero: "))
+resultado = 0
+
+for i in range(1, 13):
+    resultado = i * numero
+    print(f"{i} * {numero} = {resultado}")
---
+++ exercise3.py
@@ -0,0 +1,5 @@
+lista_palavras = ["as", "sdasajj", "asaja", "nnafas", "sdajjjj"]
+
+for palavra in lista_palavras:
+    if len(palavra) > 5:
+        print(palavra)
