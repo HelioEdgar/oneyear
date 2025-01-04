d023156b2f558cc8a14dbdd7e57ea5c24e847eed
Helio Edgar <hecoelho99@gmail.com>
2025-01-04 18:26:11 +0100

    Version 5

--- exercise2.py
+++ exercise2.py
@@ -4,4 +4,4 @@ resultado = 0
 for i in range(1, 13):
     resultado = i * numero
     print(f"{i} * {numero} = {resultado}")
-    print("========== Versão 4=============")
+    print("========== Versão 5 =============")
